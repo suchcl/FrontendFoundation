7ec4908fbce4947a016be3b79ac1f2e38cfd5b71
suchcl <ya5969@163.com>
2022-03-18 10:55:41 +0800

    promise

--- learn/promise.js
+++ learn/promise.js
@@ -1,24 +1,59 @@
-function tt(flag) {
-    function t1(resolve, reject) {
-        if (flag) {
-            var success = "hello world";
-            resolve(success);
-        } else {
-            var faild = "oooo";
-            reject(faild);
-        }
-    }
-    return new Promise(t1);
+// 封装request请求信息
+function makeRequest(url, method = "GET") {
+    let request = {
+        url: url,
+        method,
+        headers: "",
+        body: "",
+        credentials: "",
+        async: false,
+        responseType: "text",
+        referrer: ""
+    };
+    return request;
 }
 
-var r1 = tt(1);
-r1.then((res) => {
-    console.log(res);
-});
 
-r2 = tt(0);
-r2.then(res => {
-    console.log(res);
-}).catch(err => {
-    console.log(err);
+function XFetch(request) {
+    function executor(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("get", request.url, true);
+        xhr.timeout = function (e) {
+            reject(e);
+        }
+        xhr.onerror = function (e) {
+            reject(e);
+        }
+        xhr.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    resolve(this.responseText, this);
+                } else {
+                    let error = {
+                        code: this.status,
+                        response: this.response
+                    };
+                    reject(error);
+                }
+            }
+        }
+        xhr.send();
+    }
+    return new Promise(executor);
+}
+
+let p1 = XFetch(makeRequest("https://www.baidu.com"));
+let p2 = p1.then((value) => {
+    console.log(value);
+    // 其他的业务处理
+    return XFetch(makeRequest("https://www.google.com"))
 })
+let p3 = x2.then(value => {
+    console.log(value);
+    // 其他业务处理
+    return XFetch(makeRequest("https://wwww.xxxx.com"));
+})
+// 异常捕获 
+p3.catch(error => {
+    console.log(error);
+});
