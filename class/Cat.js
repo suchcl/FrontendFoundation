57ebfb71aad2e68ad3e10a55e72b221aa626f789
suchcl <ya5969@163.com>
2022-05-21 15:03:34 +0800

    class

--- class/Cat.js
+++ class/Cat.js
@@ -11,3 +11,20 @@ let c = Object.create(cat);
 c.color = "yellow";
 c.say(); // miaomiao
 console.log(c.color); // yellow
+
+
+
+var o = new Object;
+var n = new Number;
+var s = new String;
+var b = new Boolean;
+var d = new Date;
+var arg = function () { return arguments }();
+var r = new RegExp;
+var f = new Function;
+var arr = new Array;
+var e = new Error;
+console.log([o, n, s, b, d, arg, r, f, arr, e].map(v => Object.prototype.toString.call(v)));
+
+var date = new Date();
+console.log(Object.prototype.toString.call(date));
