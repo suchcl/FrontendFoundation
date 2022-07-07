c34ffe29a7992c2be55c051bd5098f0b3b8f1e0c
suchcl <ya5969@163.com>
2022-07-07 11:29:25 +0800

    类型断言：as <> 非空断言 双重断言

--- ts/type.ts
+++ ts/type.ts
@@ -2,7 +2,26 @@ let nvalue:string = "这是一个字符串";
 let strLength:number = (<string>nvalue).length;
 let strLength2:number = (nvalue as string).length;
 
-let num:number | null | undefined = 12.598374;
-num.toFixed(); // 13
-num.toFixed(2); // 12.60
-num.toFixed(4); // 12.598
+
+
+let num:number | null | undefined;
+num!.toFixed(3); // 需要把num的空类型给排除掉
+
+function sayHello(name:string | undefined){
+    let sname:string = name!; // 需要将name变量的空类型排除掉
+}
+
+interface Person{
+    name: string;
+    age: number;
+    email: string;
+}
+let student:Person = {
+    name: "Jack",
+    age: 12,
+    email: "jack@se.com"
+};
+
+// 双重断言，可以先将类型断言为any或者Unkown,都可以
+// let stu:Person = "Nicholas" as Person;
+let stu:Person = "Nicholas" as unknown as Person; // 双重类型断言，先断言为一个unknown类型（any类型也可以），然后再断言为预期的类型
