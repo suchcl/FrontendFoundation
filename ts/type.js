8b32607d15ea07260812d300f79162675b7224e9
suchcl <ya5969@163.com>
2022-08-01 13:38:09 +0800

    逻辑空赋值、空值合并运算符

--- ts/type.js
+++ ts/type.js
@@ -1,6 +1,49 @@
+var _a, _b, _c;
 var nvalue = "这是一个字符串";
 var strLength = nvalue.length;
 var strLength2 = nvalue.length;
-var num = 12.598374;
-num.toFixed(); // 13
-console.log(num.toFixed(2)); // 12.60
+var num;
+// num!.toFixed(3); // 需要把num的空类型给排除掉
+function sayHello(name) {
+    var sname = name; // 需要将name变量的空类型排除掉
+}
+var student = {
+    name: "Jack",
+    age: 12,
+    email: "jack@se.com"
+};
+// 双重断言，可以先将类型断言为any或者Unkown,都可以
+// let stu:Person = "Nicholas" as Person;
+var stu = "Nicholas"; // 双重类型断言，先断言为一个unknown类型（any类型也可以），然后再断言为预期的类型
+// const foo = null ?? "default value";
+// console.log(foo); // default value
+// const foor = undefined ?? "not default value";
+// console.log(foor); // not default value
+// const baz = 0 || 100;
+// console.log(baz); // 100
+// const bar = '' || "Hello";
+// console.log(bar); // "Hello"
+// const cnt = 0 ?? "Hello world!";
+// console.log(cnt); // 0
+var foo;
+var newFoo = foo || "hello";
+console.log(newFoo); // hello，foo声明后没有赋值，所以其值是一个undefined
+var count = 0;
+var text = "";
+var qty = count !== null && count !== void 0 ? count : 100;
+var message = text !== null && text !== void 0 ? text : 'hi';
+console.log(qty); // 0
+console.log(message); // "" 空的字符串
+var obj = {
+    msg: "hello world!"
+};
+console.log((_a = obj.msg) === null || _a === void 0 ? void 0 : _a.toUpperCase());
+var a = {
+    duration: 50,
+    speed: 10
+};
+(_b = a.duration) !== null && _b !== void 0 ? _b : (a.duration = 10);
+console.log(a.duration); // 50
+var b = (_c = a.speed) !== null && _c !== void 0 ? _c : 20;
+console.log(a.speed); // 10
+console.log(b);
--- ts/type.ts
+++ ts/type.ts
@@ -2,10 +2,8 @@ let nvalue:string = "这是一个字符串";
 let strLength:number = (<string>nvalue).length;
 let strLength2:number = (nvalue as string).length;
 
-
-
 let num:number | null | undefined;
-num!.toFixed(3); // 需要把num的空类型给排除掉
+// num!.toFixed(3); // 需要把num的空类型给排除掉
 
 function sayHello(name:string | undefined){
     let sname:string = name!; // 需要将name变量的空类型排除掉
@@ -25,3 +23,45 @@ let student:Person = {
 // 双重断言，可以先将类型断言为any或者Unkown,都可以
 // let stu:Person = "Nicholas" as Person;
 let stu:Person = "Nicholas" as unknown as Person; // 双重类型断言，先断言为一个unknown类型（any类型也可以），然后再断言为预期的类型
+
+// const foo = null ?? "default value";
+// console.log(foo); // default value
+
+// const foor = undefined ?? "not default value";
+// console.log(foor); // not default value
+
+// const baz = 0 || 100;
+// console.log(baz); // 100
+
+// const bar = '' || "Hello";
+// console.log(bar); // "Hello"
+
+// const cnt = 0 ?? "Hello world!";
+// console.log(cnt); // 0
+
+let foo;
+let newFoo = foo || "hello";
+console.log(newFoo); // hello，foo声明后没有赋值，所以其值是一个undefined
+
+let count = 0;
+let text = "";
+let qty = count ?? 100;
+let message = text ?? 'hi';
+console.log(qty); // 0
+console.log(message); // "" 空的字符串
+
+
+let obj = {
+    msg: "hello world!"
+};
+console.log(obj.msg?.toUpperCase());
+
+const a = {
+    duration: 50,
+    speed: 10
+};
+a.duration ??= 10;
+console.log(a.duration); // 50
+let b = a.speed ?? 20;
+console.log(a.speed); // 10
+console.log(b);
